096a74f0f0db2a01d34b6c4b5323182575867ca5
Seth979 <BOYDS4@my.erau.edu>
2024-11-24 15:57:46 -0700

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -1,10 +1,14 @@
+MAX_MESSAGE_LENGTH = 1024 #bytes 
 from socket import *
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM) 
 clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence:') # works only in Python 3.x
-# For Python 2.x use: message = raw_input('Input lowercase sentence:')
+sentence = input('Input lowercase sentence:')
+if len(sentence) > MAX_MESSAGE_LENGTH:
+  print(f"Error: Message length is greater than the max length")
+  clientSocket.close()
+  exit()
 clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024).decode()
 print('From Server:', modifiedSentence) 
