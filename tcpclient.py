534c776df5b404d96ecc229f802ed4f7591a9614
Seth979 <BOYDS4@my.erau.edu>
2024-11-24 15:58:26 -0700

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -5,6 +5,7 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM) 
 clientSocket.connect((serverName,serverPort))
 sentence = input('Input lowercase sentence:')
+#if the input is greater than the max length the program will give you an error and end
 if len(sentence) > MAX_MESSAGE_LENGTH:
   print(f"Error: Message length is greater than the max length")
   clientSocket.close()
