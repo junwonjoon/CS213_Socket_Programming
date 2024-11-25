084be99858f74e79995b46abc421b7c8b47d9908
Nicholas Harris <harrisn@my.erau.edu>
2024-11-24 23:30:26 -0700

    Update UDPPingerClient.py

--- Part 3/UDPPingerClient.py
+++ Part 3/UDPPingerClient.py
@@ -1,8 +1,10 @@
 # UDPPingerClient.py;
 # Reference: Kurose and Ross textbook, 7th ed.
 # Author(s): Nicholas Harris
+
 from socket import *
 import time
+import sys
 
 def calculate_rtt(client_socket, server_name, server_port, message):
     start_time = time.time()
@@ -14,15 +16,15 @@ def calculate_rtt(client_socket, server_name, server_port, message):
         print("Request timed out")
         return None 
     end_time = time.time()
-    return (end_time - start_time) * 1000, modified_message.decode()
+    return (end_time - start_time), modified_message.decode()
 
 def main():
-    server_name = ''  # Replace with the server's IP address
-    server_port = 12000
+    server_name = sys.argv[1] 
+    server_port = int(sys.argv[2])
 
     # Create a UDP socket
     client_socket = socket(AF_INET, SOCK_DGRAM)
-    
+
     # Request the user for how many messages they would like to send
     n = int(input("Enter how many messages you would like to send (typically 12): "))
 
@@ -35,34 +37,28 @@ def main():
     for i in range(n):
         message = input("Input lowercase sentence for packet %d: " %(i+1))
         rtt_result = calculate_rtt(client_socket, server_name, server_port, message)
-
         if rtt_result is None:
             timed_out += 1
             continue 
-
         current_rtt, modified_message = rtt_result
-        print(modified_message)
-
+        print("Message received from server: " + modified_message)
         if lowest_rtt is None or current_rtt < lowest_rtt:
             lowest_rtt = current_rtt
-
         if highest_rtt is None or current_rtt > highest_rtt:
             highest_rtt = current_rtt
-
         total_rtt += current_rtt
-        print("The RTT for packet %d is: %d ms" %(i+1, current_rtt))
+        print("The RTT for packet %d is: %.3f s" %(i+1, current_rtt))
 
     # Print RTT statistics
     if lowest_rtt is not None:  # Ensure at least one successful connection occurred
         print("\nRTT Statistics:")
-        print("Minimum RTT: %d ms" %(lowest_rtt))
-        print("Average RTT: %d ms" %(total_rtt/(n - timed_out)))
-        print("Maximum RTT: %d ms" %(highest_rtt))
-        packet_success_rate = (n - timed_out)/n
-        print(f"The packet loss rate is {packet_success_rate:.0%}")
+        print("Minimum RTT: %.3f s" %(lowest_rtt))
+        print("Average RTT: %.3f s" %(total_rtt/(n - timed_out)))
+        print("Maximum RTT: %.3f s" %(highest_rtt))
+        packet_loss_rate = timed_out / n
+        print(f"The packet loss rate is {packet_loss_rate:.0%}")
     else:
         print("\nNo successful connections were made.")
-    
     client_socket.close()
 
 if __name__ == "__main__":
