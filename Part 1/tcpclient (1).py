9fa1b88f2c127cf2b4c5c283af85ac7e224654ff
Wonjoon <junwonjoon41@gmail.com>
2024-12-02 15:31:42 -0700

    Added Part 1 to the GitHub

---
+++ Part 1/tcpclient (1).py
@@ -0,0 +1,16 @@
+MAX_MESSAGE_LENGTH = 1024 #bytes #can handle 1 paragrah but not 2, however it depends on how long it is
+from socket import *
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM) 
+clientSocket.connect((serverName,serverPort))
+sentence = input('Input lowercase sentence:')
+#if the input is greater than the max length the program will give you an error and end
+if len(sentence) > MAX_MESSAGE_LENGTH:
+  print(f"Error: Message length is greater than the max length")
+  clientSocket.close()
+  exit()
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024).decode()
+print('From Server:', modifiedSentence) 
+clientSocket.close()
